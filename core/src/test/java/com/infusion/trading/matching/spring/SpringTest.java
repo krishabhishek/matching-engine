c0171d1afcce055dc7eb2f6365b3b29bf1e679c9
Paul <paul.churchward@gmail.com>
2015-11-26 13:32:18 -0500

    Add a simple git status

---
+++ core/src/test/java/com/infusion/trading/matching/spring/SpringTest.java
@@ -0,0 +1,23 @@
+package com.infusion.trading.matching.spring;
+
+import com.infusion.trading.matching.test.common.TestHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+/**
+ * Created by pchurchward on 2015-11-26.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringTest{
+
+    @Autowired
+    private TestHelper testHelper;
+
+    @Test
+    public void test() {
+        assertNotNull(testHelper);
+    }
+}
