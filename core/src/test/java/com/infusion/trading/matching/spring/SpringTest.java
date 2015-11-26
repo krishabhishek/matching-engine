f902ee8776e51d072c29d68c20b0aa4b4ea996fe
Paul <paul.churchward@gmail.com>
2015-11-26 13:33:26 -0500

    Add simple test that uses SpringJunit Runner (not cucumber runner) and loads Cucumber context to assert TestHelper isn't null - works locally

--- core/src/test/java/com/infusion/trading/matching/spring/SpringTest.java
+++ core/src/test/java/com/infusion/trading/matching/spring/SpringTest.java
@@ -4,6 +4,7 @@ import com.infusion.trading.matching.test.common.TestHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,7 @@ import static org.junit.Assert.*;
  * Created by pchurchward on 2015-11-26.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:*Cucumber.xml")
 public class SpringTest{
 
     @Autowired
